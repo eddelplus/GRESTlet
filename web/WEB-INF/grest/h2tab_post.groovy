fc588f6dfadde4edf08dac1fba798e2146c9f048
Jochen <Jochen@PRUSSEIT-W7>
2013-03-31 17:44:29 +0200

    Sanitize table name in sample

--- web/WEB-INF/grest/h2tab_post.groovy
+++ web/WEB-INF/grest/h2tab_post.groovy
@@ -6,10 +6,11 @@ if (!(request.pathvar1 ==~ /\w+/)) {
 }
 
 def sql = H2Pool.getSql(context)
+def tabname = request.pathvar1.toUpperCase()
 
 if (data.ID?.isBigInteger()) {
     sql.resultSetConcurrency = java.sql.ResultSet.CONCUR_UPDATABLE
-    sql.eachRow('select * from ' + request.pathvar1.toUpperCase() + ' where ID=' + data.ID) { row ->
+    sql.eachRow('select * from ' + tabname + ' where ID=' + data.ID) { row ->
         data.each { fld, val -> row[fld] = val }
     }
 }
