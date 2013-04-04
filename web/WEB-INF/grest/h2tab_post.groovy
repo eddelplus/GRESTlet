5826588fa8bbe984d58cf1e1a1d884f346c3f395
Jochen Eddelbüttel <jochen@eddelbuettel.net>
2013-04-05 00:21:56 +0300

    Update h2tab_post.groovy

--- web/WEB-INF/grest/h2tab_post.groovy
+++ web/WEB-INF/grest/h2tab_post.groovy
@@ -10,9 +10,10 @@ def tabname = request.pathvar1.toUpperCase()
 
 if (data.ID) {
     sql.resultSetConcurrency = java.sql.ResultSet.CONCUR_UPDATABLE
-    sql.eachRow('select * from ' + tabname + ' where ID=' + data.ID.toBigInteger()) { row ->
-        data.each { fld, val -> row[fld] = val }
-    }
+    def md
+    sql.eachRow('select * from ' + tabname + ' where ID=' + data.ID.toBigInteger())
+        { md = it }
+        { for (i in 1..md.columnCount) it[md.getColumnName(i)] = data[md.getColumnName(i)] }
 }
 else {
     data.ID = sql.firstRow('select ' + tabname + '_SEQ.NEXTVAL as ID from DUAL').ID
